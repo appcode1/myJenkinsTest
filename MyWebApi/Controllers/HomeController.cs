3265d9b958af15c40a53de2afb08feafaac1562f
bibletrccc <spacedna@hotmail.com>
2026-07-03 17:02:31 -0400

    init

---
+++ MyWebApi/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return $"Welcome! This is the HomeController of MyWebApi. {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}";
+        }
+    }
+}
