2ee20e27ff16be5c658b9024fa232ff77dcec87c
bibletrccc <spacedna@hotmail.com>
2026-07-04 00:23:40 -0400

    add userController

---
+++ MyWebApi/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return $"select user. {DateTime.Now}";
+        }
+    }
+}
