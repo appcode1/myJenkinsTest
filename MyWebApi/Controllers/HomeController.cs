f1ddf0765d181f547bef22769df03a275a6edd0e
bibletrccc <spacedna@hotmail.com>
2026-07-06 13:53:06 -0400

    update

--- MyWebApi/Controllers/HomeController.cs
+++ MyWebApi/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MyWebApi.Controllers
         [HttpGet]
         public string Get()
         {
-            return $"testing deployment again...create the deployment yaml file. {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}";
+            return $"Kubectl...deployment.create the deployment yaml file. {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}";
         }
     }
 }
